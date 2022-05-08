969325dd4e56c163a73c12bf676b647b9030262a
ChadH28 <chadhoosain@gmail.com>
2022-05-08 19:03:31 +0200

    Used bootstrap for css classes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 
+import PageLayout from '../components/PageLayout'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
+  )
 }
 
 export default MyApp
